0ad377e28958374bc6fd5dff9f496bffc3682f7a
AkshatTripathi77 <gamingx123456@gmail.com>
2026-02-05 19:41:55 +0530

    variable practice

--- New.py
+++ New.py
@@ -1,2 +1,7 @@
 print("Hello World")
 print("Hello Akshat Tripathi")
+a = "Akshat"
+b = "cst"
+print (a , b)
+print ( 1 * 3 + 3 - 4)
+print(10*" Akshat")
---
+++ Variables.py
@@ -0,0 +1,13 @@
+nums = [23, 45, 67, 89, 10]
+print(nums[1])
+names = ['Akshat','Ananya','CHanda','KEshav']
+mil = [nums,names]
+print(mil)
+nums.append(17)
+print(nums)
+nums.insert(3,56)
+print(nums)
+nums.pop(2)
+print(nums)
+nums.pop()
+print(nums)
