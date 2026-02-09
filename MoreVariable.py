102d6a8a850ccf4ded173aa7ab63f9e9ba9e5fac
AkshatTripathi77 <gamingx123456@gmail.com>
2026-02-09 16:44:21 +0530

    Set,Dictionary practice

---
+++ MoreVariable.py
@@ -0,0 +1,14 @@
+num = 5
+print(id(num))
+name = 'Akshat'
+print(id(name))
+a = 10
+b = a
+print(id(a))
+print(id(b))
+a = 9
+print(id(a))
+print(id(b))
+k = a
+print(id(k))
+print(type(a))
---
+++ dataTypes.py
@@ -0,0 +1,24 @@
+num = 2.5
+print(type(num))
+num = 5
+print(type(num))
+num = 6+9j
+print(type(num))
+a = 5.6
+b = int(a)
+print(b)
+k = float(b)
+print(k)
+k = 6
+c = complex(b,k)
+print(c)
+s = True
+print(type(s))
+l = [12,34,56,78]
+print(type(l))
+s = {12,34,56,78,90}
+print(type(s))
+t = (123,342,125,6567)
+print(type(t))
+print(t)
+print(s)
